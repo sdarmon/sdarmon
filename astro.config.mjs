e9fa3d9f799c6969b094a3391092e1fabb8aae6a
sdarmon <sasha.darmon@univ-lyon1.fr>
2026-04-03 18:38:11 +0200

    Fix: désactivation Jekyll et passage à Node 24

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    // REMPLACE par ton pseudo github et le nom de ton dépôt
     site: 'https://sdarmon.github.io',
-    base: '/portfolio-sasha', // TRÈS IMPORTANT si ton dépôt n'est pas "pseudo.github.io"
 });
