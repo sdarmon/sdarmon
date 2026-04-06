683adaacb81e38b3c4e63e87a25dddbee42c3fcd
sdarmon <sasha.darmon@univ-lyon1.fr>
2026-04-07 01:29:55 +0200

    Update : English version

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  fr: 'Français',
+  en: 'English',
+};
+
+export const ui = {
+  fr: {
+    'nav.home': 'Accueil',
+    'nav.ateliers': 'Ateliers de médiation',
+    'nav.contact': 'Contact',
+    'nav.com': 'Communication & Prix',
+    'nav.publi': 'Publications',
+    'nav.cv' : 'Mon CV',
+    'footer.property': 'Mentions Légales',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.ateliers': 'Science Workshops',
+    'nav.contact': 'Contact Me',
+    'nav.com': 'Communication & Rewards',
+    'nav.publi': 'Publications',
+    'nav.cv': 'Resume',
+    'footer.property': 'Legal Notice',
+  },
+} as const;
