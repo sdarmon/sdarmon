8f0f6ffb36626ba039c3dc77b31399622d9a439d
sdarmon <sasha.darmon@univ-lyon1.fr>
2026-04-07 01:30:47 +0200

    Update : English version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,12 @@ import sitemap from '@astrojs/sitemap'; // AJOUT
 
 export default defineConfig({
     site: 'https://sashadarmon.fr',
+    i18n: {
+        defaultLocale: 'fr',
+        locales: ['fr', 'en'],
+        routing: {
+            prefixDefaultLocale: false // 'fr' reste à la racine, 'en' sera dans /en/
+        }
+    },
     integrations: [sitemap()], // AJOUT
 });
